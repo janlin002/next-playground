8165b307f38f6526bd352e14aa3054df9b8584e0
janlin002 <123davidbill@gmail.com>
2024-03-25 16:56:47 +0800

    feat: add i18n

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 // import { plugins } from './build/plugins'
+const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
   reactStrictMode: true,
   // plugins: plugins()
-}
+  i18n,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -8,14 +8,16 @@ import {
 } from "firebase/remote-config";
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STOREAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STOREAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// };
+
+const firebaseConfig = {};
 
 const useRemoteConfig = (key: string) => {
   const [message, setMessage] = useState<string | null>(null);
@@ -23,12 +25,13 @@ const useRemoteConfig = (key: string) => {
   useEffect(() => {
     if (typeof window !== "undefined") {
       const app = initializeApp(firebaseConfig);
-      const remoteConfig = getRemoteConfig(app);
-      const isFetched: any = fetchAndActivate(remoteConfig);
+      // const remoteConfig = getRemoteConfig(app);
+      // const isFetched: any = fetchAndActivate(remoteConfig);
+      const isFetched: any = false;
 
       if (isFetched) {
-        const searchMessage = getString(remoteConfig, key);
-        setMessage(searchMessage);
+        // const searchMessage = getString(remoteConfig, key);
+        // setMessage(searchMessage);
       } else {
         console.error("Failed to fetch remote config");
       }
